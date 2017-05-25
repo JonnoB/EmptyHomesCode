a03ab1febe57e96f5d97f43dc3b963129baba189
Jonathan Bourne <cheg_a_leg@hotmail.com>
2017-05-25 18:02:39 +0100

    Remove DAT
    Changed the way maps are plotted so the DAT dataframe is no longer needed. It also allows for all data on a region to be plotted even when that data is missing

--- Functions/PlotMap.R
+++ Functions/PlotMap.R
@@ -1,12 +1,17 @@
-PlotMap <- function(df, name, variable = "LowUsePerc", title = "Map of Region"){
+PlotMap <- function(df, ShapeData, variable = "LowUsePerc", title = "Map of Region", filtermap = TRUE){
   
+  
+  #ONLY SET FITLER MAP TO FALSE WHEN the shape FILES HAVE BEEN PRE FILTERED OTHERWISE PUNISHMENT AND REGRET
   value <- mean(eval(parse(text= paste0("df$", variable))), na.rm = TRUE)
   
-  Singlevis <- AG %>% filter(grepl(name, LAD11NM)) %>% 
-    left_join(., select(df, LSOA_CODE, LowUsePerc, WardLowUsePerc, LowUse, Empty, Homes), 
+  Singlevis <- ShapeData %>% 
+    left_join(., select(df, LSOA_CODE, LowUsePerc, WardLowUsePerc, LowUse, Empty, Homes),
               by = c("lsoa11cd"="LSOA_CODE")) %>% 
-    ungroup 
+        ungroup
   
+  if(filtermap){
+  Singlevis <- Singlevis %>% filter(!is.na(LowUsePerc))
+  }
   ggplot(Singlevis, aes_string("long", "lat", group = "group", fill = variable))+ 
     geom_polygon(colour = alpha("black", 1/2), size = 0.2) + 
     scale_fill_gradient2(low = "blue", mid = "white", high = "red", midpoint = value)+
