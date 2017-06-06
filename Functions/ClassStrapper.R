30f698068eee7a72f6aa5a0a26012737f33a92a9
Jonathan Bourne <cheg_a_leg@hotmail.com>
2017-06-06 14:29:26 +0100

    Bootsrapped results
    Worked through the bootstrapping process unpicking errors that had been made on the first attempt.

--- Functions/ClassStrapper.R
+++ Functions/ClassStrapper.R
@@ -1,15 +1,15 @@
-ClassStrapper <- function(PriceData, LowUseCounts){
-  SampBlock <- sample(1:nrow(PriceData), LowUseCounts*1000, replace = TRUE ) %>% 
-    matrix(data = ., ncol = 1000)
+ClassStrapper <- function(PriceData, LowUseCounts, reps = 1000){
   
-  Out <- PriceData[,3] %>% 
-    slice(SampBlock[,1]) %>%
-    group_by(class) %>%
-    summarise(Counts = n()) %>%
-    mutate(ID = 1)
+  #PriceData: A data frame of three columns, the LSOA code, the price of the sale, the class of the sale
+  #LowUseCounts: The number of low use homes in that local authority.
+  #reps: The number of samples wanted
   
+  #generate random indexes to sample
+  SampBlock <- sample(1:nrow(PriceData), LowUseCounts*reps, replace = TRUE ) %>% 
+    matrix(data = ., ncol = reps)
   
-  Out <- 1:1000 %>% map_df(~PriceData[,3] %>% 
+
+  Out <- 1:reps %>% map_df(~PriceData[,"class"] %>% 
                              slice(SampBlock[,.x]) %>%
                              group_by(class) %>%
                              summarise(Counts = n()) %>%
